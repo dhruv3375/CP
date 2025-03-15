07380e3bcc96fd499ae82881556e0b9071ed6910
Dhruv Patel <pateldhruv2351@gmail.com>
2025-03-15 17:34:46 +0530

    Day 13

--- CPP Basics/X_Two_intervals.cpp
+++ CPP Basics/X_Two_intervals.cpp
@@ -4,20 +4,40 @@ using namespace std;
 int main()
 {
     int l1 , r1 , l2 , r2;
-
     cin >> l1 >> r1 >> l2 >> r2;
 
-    int i , j;
-    i = max(l1 , l2);
-    j = min(r1 , r2);
-
-    if(i > j)
+    if(l2 >= l1)
     {
-        cout << -1;
+        if(l2 <= r1){
 
+            if(r2 <= r1)
+            {
+                cout<<l2 <<" "<<r2<<endl;
+            }
+            else{
+                cout << l2<<" "<<r1<<endl;
+            }
+        }
+
+        else{
+            cout <<-1<<endl;
+        }
     }
-    else {
-        cout << i <<" "<<j<<endl;
+    else{
+        if(l1 <= r2)
+        {
+            if(r1 <= r2)
+            {
+                cout << l1 <<" "<<r1<<endl;
+            }
+            else{
+                cout <<l1 <<" "<<r2<<endl;
+            }
+        }
+        else{
+            cout <<-1<<endl;
+        }
     }
+
     return 0;
 }
